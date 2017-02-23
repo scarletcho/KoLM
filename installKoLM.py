4db52d2c6e1eb8b98c255c58e53a41cfae759cda
Yejin Cho <scarletcho@gmail.com>
2017-02-23 14:13:13 +0900

    Packaged and registered to PyPI.

--- installKoLM.py
+++
@@ -1,4 +0,0 @@
-# Install KoLM package by locating kolm directory to user's site-packages path
-import os
-import site; path = site.getsitepackages()[0]
-os.system('cp -rf kolm ' + path)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+print('Setting up KoLM...')
+setup(
+    name = "kolm",
+    version = "1.0.2",
+    description = "Korean LM toolkit for building ASR system",
+    author = "Yejin Cho",
+    author_email = "scarletcho@gmail.com",
+    keywords = "Korean Language Modeling Toolkit",
+    url = "https://github.com/scarletcho/KoLM",
+    packages = ['kolm'],
+    install_requires = [
+    	'JPype1',
+    	'konlpy',
+    	'korean',
+    	'hanja'],
+    dependency_links=['https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh'],
+    include_package_data = True,
+    package_data = {
+        '': ['*.txt'],
+    }
+)
